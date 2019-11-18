d58fecbc4e1f85785c5fb8ab6d361be40443b0a0
jgomezgar <44750387+jgomezgar@users.noreply.github.com>
2019-11-18 13:08:07 +0100

    Update 01.ITG_ACTIVITIES.sql

--- DataPrep/01.ITG_ACTIVITIES.sql
+++ DataPrep/01.ITG_ACTIVITIES.sql
@@ -22,11 +22,13 @@ from	ITE.Fact_Invest_Actuals_Daily	a11
 	
 	join	ITE.V_LU_BRANDFAMILIES	a18
 	  on 	(a13.[BRANDFAMILY_ID] = a18.[BRANDFAMILY_ID])
---where	(a16.[MES_ID] = convert(int,convert(varchar,getdate()-1,112))/100
- where a14.[Midcategory] in (N'BLOND', N'RYO')
+where	a15.CAL_MONTH  >= '201601'
+ and  a14.[Midcategory] in (N'BLOND', N'RYO')
 group by	a15.[CAL_DATE]  ,
 a11.[Customer_ID]  ,
 a13.[BRANDFAMILY_ID]  ,
-	a11.[Investment_type]  ,
-	a12.[Item_type]  ,
+	a11.[Investment_type],
+	a12.[Item_type],
 	a12.[Concepto] 
+	
+	order by 2,3,1
