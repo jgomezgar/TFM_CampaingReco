4bede6649315ab20d397a36fb6aca0f8b9ebe670
jgomezgar <44750387+jgomezgar@users.noreply.github.com>
2019-11-18 19:34:31 +0100

    Update 02.ITG_SELL_OUT_btwn_IN.sql

--- 00_Extraction/00_AuxSQL/02.ITG_SELL_OUT_btwn_IN.sql
+++ 00_Extraction/00_AuxSQL/02.ITG_SELL_OUT_btwn_IN.sql
@@ -1,27 +1,10 @@
-/*with ITG_Sell_OUT as (
-select	a16.[CAL_DATE] SO_DATE,
-	a11.[CUSTOMER_ID]  CUSTOMER_ID,
-	a13.[BRANDFAMILY_ID]  BRANDFAMILY_ID,
-	a13.[SUBCATEGORY],
-	a11.[Volumen] / a110.[CONVERSION_FACTOR]  SO_WSE,
-	min(a16.[CAL_DATE]) over (partition by a11.[CUSTOMER_ID], a13.[BRANDFAMILY_ID] ) SO_Start,
-	max(a16.[CAL_DATE]) over (partition by a11.[CUSTOMER_ID], a13.[BRANDFAMILY_ID] ) SO_End
 
-from	ITE.Fact_SO_Logista_smld_Custproduct	a11
-	join	ITE.T_PRODUCTS	a12
-	  on 	(a11.[Product_id] = a12.[Product_id])
-	join	ITE.T_BRANDPACKS	a13
-	  on 	(a12.[BRANDPACK_ID] = a13.[BRANDPACK_ID])
-	join	ITE.T_DAY	a16
-	  on 	(a11.[DIA] = a16.[DIA])
-	join	ITE.fact_conversion_factor	a110
-	  on 	(a13.[CATEGORY] = a110.[CATEGORY] and 
-			a13.[EXPANDED] = a110.[EXPANDED] and 
-			a13.[PACKTYPE] = a110.[PACKTYPE])
-where a16.CAL_MONTH >=  '201601'
-  and a13.[SUBCATEGORY] in (N'BLOND', N'RYO')
-)
-*/
+/*#########################################################################*/
+IF OBJECT_ID('[STAGING_2].[dbo].XXX_Sell_IN_Periods', 'U') IS NOT NULL
+ DROP TABLE [STAGING_2].[dbo].XXX_Sell_IN_Periods;
+ 
+ /* posible mejora con el tratamiento de negativos */
+ 
 select	SI.r -1 R,
 		SI.CAL_DATE,
 		SI.CAL_DATE_end,
@@ -30,7 +13,59 @@ select	SI.r -1 R,
 		SI.Midcategory,
 --		SI.SI_WSE,
 		SI.SI_ITG_WSE,
-		ceiling(SUM([Mrkt_WSE])) SI_MRKT_WSE,
+		ceiling(SUM([Mrkt_WSE] - SUM(ITG_WSE) + SI.SI_ITG_WSE)) SI_MRKT_WSE
+   	--    ceiling(SUM(case when SI.BRANDFAMILY_ID =	SO.BRANDFAMILY_ID  then SO.SO_WSE end)) SO_ITG_WSE,
+  	 --   ceiling(SUM( SO.SO_WSE)) SO_MRKT_WSE
+
+  	    
+--		SI.SI_WSE_median,
+--		SI.HI,
+--		SI.std,
+--		SI.mean
+Into [STAGING_2].[dbo].XXX_Sell_IN_Periods
+from [STAGING_2].[dbo].XXX_ITG_Sell_IN_Periods SI
+join [ITE_PRD].[ITE].[V_FACT_Sales_Target_WSE_Daily] m
+	   on SI.CUSTOMER_ID 	= m.CUSTOMER_ID and
+		  SI.Midcategory =	m.Midcategory and
+		  convert(date,convert(varchar,m.[DIA]),112) >= SI.CAL_DATE and
+		  convert(date,convert(varchar,m.[DIA]),112) < SI.CAL_DATE_end  
+/*left join [STAGING_2].[dbo].XXX_ITG_Sell_OUT SO 
+	   on SI.CUSTOMER_ID 	= SO.CUSTOMER_ID and
+		  --SI.BRANDFAMILY_ID =	SO.BRANDFAMILY_ID and
+		  SI.Midcategory =	SO.[SUBCATEGORY] and
+		  SO_DATE > SI.CAL_DATE and
+		  SO_DATE <= SI.CAL_DATE_end  
+
+where  SI.CAL_DATE between SO_Start and SO_End */
+where isnull([Mrkt_WSE],0) > 0
+GROUP by 
+		SI.r,
+		SI.CAL_DATE,
+		SI.CAL_DATE_end,
+		SI.CUSTOMER_ID,
+		SI.BRANDFAMILY_ID,
+		SI.Midcategory,
+		SI.SI_ITG_WSE
+order by SI.CUSTOMER_ID,
+		SI.BRANDFAMILY_ID,
+		SI.CAL_DATE
+		  
+
+
+/*#########################################################################*/
+IF OBJECT_ID('[STAGING_2].[dbo].XXX_Sell_Periods', 'U') IS NOT NULL
+ DROP TABLE [STAGING_2].[dbo].XXX_Sell_Periods;
+ 
+ 
+select	SI.r -1 R,
+		SI.CAL_DATE,
+		SI.CAL_DATE_end,
+		SI.CUSTOMER_ID,
+		SI.BRANDFAMILY_ID,
+		SI.Midcategory,
+--		SI.SI_WSE,
+		SI.SI_ITG_WSE,
+		ceiling(SUM(SI_MRKT_WSE)) SI_MRKT_WSE,
    	    ceiling(SUM(case when SI.BRANDFAMILY_ID =	SO.BRANDFAMILY_ID  then SO.SO_WSE end)) SO_ITG_WSE,
   	    ceiling(SUM( SO.SO_WSE)) SO_MRKT_WSE
 
@@ -40,12 +75,12 @@ select	SI.r -1 R,
 --		SI.std,
 --		SI.mean
 Into [STAGING_2].[dbo].XXX_Sell_Periods
-from [STAGING_2].[dbo].XXX_ITG_Sell_IN_Periods SI
-join [ITE_PRD].[ITE].[V_FACT_Sales_Target_WSE_Daily] m
+from [STAGING_2].[dbo].XXX_Sell_IN_Periods SI
+/*join [ITE_PRD].[ITE].[V_FACT_Sales_Target_WSE_Daily] m
 	   on SI.CUSTOMER_ID 	= m.CUSTOMER_ID and
 		  SI.Midcategory =	m.Midcategory and
 		  convert(date,convert(varchar,m.[DIA]),112) >= SI.CAL_DATE and
-		  convert(date,convert(varchar,m.[DIA]),112) < SI.CAL_DATE_end  
+		  convert(date,convert(varchar,m.[DIA]),112) < SI.CAL_DATE_end  */
 left join [STAGING_2].[dbo].XXX_ITG_Sell_OUT SO 
 	   on SI.CUSTOMER_ID 	= SO.CUSTOMER_ID and
 		  --SI.BRANDFAMILY_ID =	SO.BRANDFAMILY_ID and
@@ -65,5 +100,4 @@ GROUP by
 		SI.SI_ITG_WSE
 order by SI.CUSTOMER_ID,
 		SI.BRANDFAMILY_ID,
-		SI.CAL_DATE
-		  
+		SI.CAL_DATE 
