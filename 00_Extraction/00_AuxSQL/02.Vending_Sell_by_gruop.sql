6be0ffa17ce3d08bfff99bca448ddfc2d75d6446
jgomezgar <44750387+jgomezgar@users.noreply.github.com>
2019-11-21 16:16:39 +0100

    Update 02.Vending_Sell_by_gruop.sql

--- 00_Extraction/00_AuxSQL/02.Vending_Sell_by_gruop.sql
+++ 00_Extraction/00_AuxSQL/02.Vending_Sell_by_gruop.sql
@@ -1,7 +1,7 @@
 with vending as (
 select 
 	a13.[MES_ID] MES,
-	SUBSTRING(c.PROVINCE_TR_ID,1,2) T_PROV,
+	SUBSTRING(c.PROVINCE_TR_ID,1,2) PROV,
 --	v.PROVINCE_ID V_PROV,
 	c.POSTALCODE T_CP,
 --	 SUBSTRING(v.CP,1,2) p,
@@ -23,19 +23,37 @@ join [ITE_PRD].[ITE].[LU_CLTE_1CANAL] c
 join ITE.V_Transf_MES_MQT1	a13
   on (v.[MES] = a13.[MQT1])
 
-where [ITG_SV_vol] is not null and a13.[MES_ID] < 201911 
+where [ITG_SV_vol] is not null and [Mrkt_SV_vol] > 0 and 
+      a13.[MES_ID] < 201911 
 )  
 
 
 ---#######################################
-, CUSTOMER AS (
+, CLTE AS (
+select 
+  MES, 
+  CUSTOMER_ID,
+  BRANDFAMILY_ID,	
+  sum(ITG_SV_vol) ITG_SV_vol,
+  sum(Mrkt_SV_vol) Mrkt_SV_vol,
+  sum(ITG_SV_vol) / sum(Mrkt_SV_vol) clt_VShare
+from vending
+group by 
+  MES, 
+  CUSTOMER_ID,
+  BRANDFAMILY_ID
+)
 
+---#######################################
+, CP AS (
 select 
 	MES, 
 	CP,
 	BRANDFAMILY_ID,	
 	SUM(ITG_SV_vol) ITG_SV_vol,
-	SUM(Mrkt_SV_vol) Mrkt_SV_vol
+	SUM(Mrkt_SV_vol) Mrkt_SV_vol,
+	sum(ITG_SV_vol) / sum(Mrkt_SV_vol) CP_VShare
+
 		FROM (
 			select 
 				MES, 
@@ -60,15 +78,41 @@ select
 			CP,
 			BRANDFAMILY_ID
 )
-
+---#######################################
+, PROV AS (
 select 
   MES, 
-  T_PROV,
+  PROV,
   BRANDFAMILY_ID,	
   sum(ITG_SV_vol) ITG_SV_vol,
-  sum(Mrkt_SV_vol) Mrkt_SV_vol
+  sum(Mrkt_SV_vol) Mrkt_SV_vol,
+  sum(ITG_SV_vol) / sum(Mrkt_SV_vol) Prov_VShare
 from vending
 group by 
   MES, 
-  T_PROV,
+  PROV,
   BRANDFAMILY_ID
+)
+  
+  SELECT c.[Customer_ID],
+  	SUBSTRING(c.PROVINCE_TR_ID,1,2) PROV,
+  	c.POSTALCODE CP,
+  	[Mes_ID],
+  	[Sales2C_num],
+  	[predic2C],
+  	clt_VShare,
+  	cp_VShare,
+  	Prov_VShare
+  FROM [ITE_PRD].[ITE].[LU_CLTE_1CANAL] c
+  left join [STAGING_2].[dbo].[XXX_Vending_Predic_Prop]v
+  on v.CUSTOMER_ID=c.CUSTOMER_ID 
+  left join CLTE 
+	on (c.[Customer_ID] = CLTE.[Customer_ID]
+		and CLTE.MES = v.Mes_ID)	
+  left join CP 
+	on c.POSTALCODE = CP.CP
+	  and CP.MES = v.Mes_ID
+  left join PROV 
+	on PROV.PROV=SUBSTRING(c.PROVINCE_TR_ID,1,2)
+		and PROV.MES = v.Mes_ID
+  
