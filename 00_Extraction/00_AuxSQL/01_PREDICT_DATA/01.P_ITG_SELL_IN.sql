8e478465ccec777afd4fdf8be2d01c17fca4bc8b
jgomezgar <44750387+jgomezgar@users.noreply.github.com>
2020-01-07 22:32:58 +0100

    Update 01.P_ITG_SELL_IN.sql

--- 00_Extraction/00_AuxSQL/01_PREDICT_DATA/01.P_ITG_SELL_IN.sql
+++ 00_Extraction/00_AuxSQL/01_PREDICT_DATA/01.P_ITG_SELL_IN.sql
@@ -1,46 +1,15 @@
 /***********   10 min *****************/
 /*  RESULT: Datos entrenamiento ventas SellIn 
-   [STAGING_2].[dbo].XXX_ITG_Sell_OUT  --> Sell Out WSE y fechas de validec de estancos, delita los erstancos Validos
-   [STAGING_2].[dbo].XXX_ITG_Sell_IN --> Sell In Positivos, ventas base
-   [STAGING_2].[dbo].XXX_ITG_Sell_IN_neg --> Sell In Negativos, determina Familias&Clitentes a arreglar
-   [STAGING_2].[dbo].XXX_ITG_Sell_IN_Periods -->Sell In de estancos Sell Out, arreglados los negativos, y agrupados por sacas.
+   [STAGING_2].[dbo].XXX_P_ITG_Sell_IN --> Sell In Positivos, ventas base
+   [STAGING_2].[dbo].XXX_P_ITG_Sell_IN_neg --> Sell In Negativos, determina Familias&Clitentes a arreglar
+   [STAGING_2].[dbo].XXX_P_ITG_Sell_IN_Periods -->Sell In de estancos SIN Sell Out, arreglados los negativos, y agrupados por sacas.
 */
 
-/*#########################################################################*/
-IF OBJECT_ID('[STAGING_2].[dbo].XXX_ITG_Sell_OUT', 'U') IS NOT NULL
- DROP TABLE [STAGING_2].[dbo].XXX_ITG_Sell_OUT;
-
-
-select	a16.[CAL_DATE] SO_DATE,
-	a11.[CUSTOMER_ID]  CUSTOMER_ID,
-	a13.[BRANDFAMILY_ID]  BRANDFAMILY_ID,
-	a13.[SUBCATEGORY],
-	sum(a11.[Volumen] / a110.[CONVERSION_FACTOR])  SO_WSE,
-	min(a16.[CAL_DATE]) over (partition by a11.[CUSTOMER_ID], a13.[BRANDFAMILY_ID] ) SO_Start,
-	max(a16.[CAL_DATE]) over (partition by a11.[CUSTOMER_ID], a13.[BRANDFAMILY_ID] ) SO_End
-into [STAGING_2].[dbo].XXX_ITG_Sell_OUT
-from	ITE_PRD.ITE.Fact_SO_Logista_smld_Custproduct	a11
-	join	ITE.T_PRODUCTS	a12
-	  on 	(a11.[Product_id] = a12.[Product_id])
-	join	ITE.T_BRANDPACKS	a13
-	  on 	(a12.[BRANDPACK_ID] = a13.[BRANDPACK_ID])
-	join	ITE.T_DAY	a16
-	  on 	(a11.[DIA] = a16.[DIA])
-	join	ITE.fact_conversion_factor	a110
-	  on 	(a13.[CATEGORY] = a110.[CATEGORY] and 
-			a13.[EXPANDED] = a110.[EXPANDED] and 
-			a13.[PACKTYPE] = a110.[PACKTYPE])
-where a16.CAL_MONTH >=  '201710'
-  and a13.[SUBCATEGORY] in (N'BLOND', N'RYO')
-group by a16.[CAL_DATE],
-		 a11.[CUSTOMER_ID],
-		 a13.[BRANDFAMILY_ID],
-		 a13.[SUBCATEGORY]
-
+)
 
 /*#########################################################################*/
-IF OBJECT_ID('[STAGING_2].[dbo].XXX_ITG_Sell_IN', 'U') IS NOT NULL
- DROP TABLE [STAGING_2].[dbo].XXX_ITG_Sell_IN;
+IF OBJECT_ID('[STAGING_2].[dbo].XXX_P_ITG_Sell_IN', 'U') IS NOT NULL
+ DROP TABLE [STAGING_2].[dbo].XXX_P_ITG_Sell_IN;
  
 select	
 	a12.CAL_DATE,
@@ -48,17 +17,16 @@ select
 	a15.[BRANDFAMILY_ID]  BRANDFAMILY_ID,
 	a15.SUBCATEGORY  Midcategory,
 	sum(a11.[VOL_EQUI])  SI_WSE
-into [STAGING_2].[dbo].XXX_ITG_Sell_IN
+into [STAGING_2].[dbo].XXX_P_ITG_Sell_IN
 from	ITE.FACT_SMLD_Smoke_ITG	a11
 	join	ITE.T_DAY	a12
 	  on 	(a11.[SALESDATE] = a12.[CAL_DAY])
 	join	ITE.T_BRANDPACKS	a15
 	  on 	(a11.[BRANDPACK_ID] = a15.[BRANDPACK_ID] )
-where	a12.CAL_MONTH >=  '201709' and
- exists (select 1 from   [STAGING_2].[dbo].XXX_ITG_Sell_OUT SO 
-				   where a11.CUSTOMER_ID 	= SO.CUSTOMER_ID and
-					  a15.BRANDFAMILY_ID =	SO.BRANDFAMILY_ID and
-					  a12.CAL_DATE between SO_Start and SO_End	 )
+where	a12.CAL_MONTH >=  '201500' /*and
+ not exists (select 1 from   ITE.V_Lu_Muestra_SO_1Canal_15M SO 
+				   where  SO.[Muestra_so_ok]=1 and 
+				   a11.CUSTOMER_ID 	= SO.CUSTOMER_ID )*/
  and a15.SUBCATEGORY in (N'BLOND', N'RYO')
  and a11.[VOL_EQUI] > 0
 group by	CAL_DATE,
@@ -68,8 +36,8 @@ group by	CAL_DATE,
 
 	
 /*#########################################################################*/
-IF OBJECT_ID('[STAGING_2].[dbo].XXX_ITG_Sell_IN_neg', 'U') IS NOT NULL
- DROP TABLE [STAGING_2].[dbo].XXX_ITG_Sell_IN_neg;
+IF OBJECT_ID('[STAGING_2].[dbo].XXX_P_ITG_Sell_IN_neg', 'U') IS NOT NULL
+ DROP TABLE [STAGING_2].[dbo].XXX_P_ITG_Sell_IN_neg;
  
 select	
 	  CAL_DATE,
@@ -77,18 +45,17 @@ select
 	a15.[BRANDFAMILY_ID]  BRANDFAMILY_ID,
 	a15.SUBCATEGORY  Midcategory,
 	ceiling(sum(a11.[VOL_EQUI]))  SI_WSE
-into [STAGING_2].[dbo].XXX_ITG_Sell_IN_neg
+into [STAGING_2].[dbo].XXX_P_ITG_Sell_IN_neg
 from	ITE.FACT_SMLD_Smoke_ITG	a11
 	join	ITE.T_DAY	a12
 	  on 	(a11.[SALESDATE] = a12.[CAL_DAY])
 	join	ITE.T_BRANDPACKS	a15
 	  on 	(a11.[BRANDPACK_ID] = a15.[BRANDPACK_ID] )
 
-where	a12.CAL_MONTH >=  '201709'  and
- exists (select 1 from   [STAGING_2].[dbo].XXX_ITG_Sell_OUT SO 
-				   where a11.CUSTOMER_ID 	= SO.CUSTOMER_ID and
-					  a15.BRANDFAMILY_ID =	SO.BRANDFAMILY_ID and
-					  a12.CAL_DATE between SO_Start and SO_End	 )
+where	a12.CAL_MONTH >=  '201500' /*  and
+ not exists (select 1 from   ITE.V_Lu_Muestra_SO_1Canal_15M SO 
+				   where  SO.[Muestra_so_ok]=1 and 
+				   a11.CUSTOMER_ID 	= SO.CUSTOMER_ID ) */
  and a15.SUBCATEGORY in (N'BLOND', N'RYO')
  and a11.[VOL_EQUI] < 0
 group by	CAL_DATE,
@@ -99,8 +66,8 @@ group by	CAL_DATE,
 
 
 /*################################ 33 min #####################################*/
-IF OBJECT_ID('[STAGING_2].[dbo].XXX_ITG_Sell_IN_Periods', 'U') IS NOT NULL
- DROP TABLE [STAGING_2].[dbo].XXX_ITG_Sell_IN_Periods;
+IF OBJECT_ID('[STAGING_2].[dbo].XXX_P_ITG_Sell_IN_Periods', 'U') IS NOT NULL
+ DROP TABLE [STAGING_2].[dbo].XXX_P_ITG_Sell_IN_Periods;
  
  
 
@@ -121,8 +88,8 @@ with ITG_Sell_IN_std as (
 				ceiling(avg(t.SI_WSE) over (partition by  t.[CUSTOMER_ID], t.[BRANDFAMILY_ID],n.CAL_DATE)) mean,
 				min(t.CAL_DATE) over (partition by  t.[CUSTOMER_ID], t.[BRANDFAMILY_ID],n.CAL_DATE) Date_Start,
 				max(t.CAL_DATE) over (partition by  t.[CUSTOMER_ID], t.[BRANDFAMILY_ID],n.CAL_DATE) Date_End
-			from [STAGING_2].[dbo].XXX_ITG_Sell_IN t
-			 join [STAGING_2].[dbo].XXX_ITG_Sell_IN_neg n 
+			from [STAGING_2].[dbo].XXX_P_ITG_Sell_IN t
+			 join [STAGING_2].[dbo].XXX_P_ITG_Sell_IN_neg n 
 				   on t.[CUSTOMER_ID]=n.[CUSTOMER_ID] and  
 					  t.[BRANDFAMILY_ID]= n.[BRANDFAMILY_ID] and
 					  n.CAL_DATE > t.CAL_DATE and
@@ -164,8 +131,8 @@ select  --t.r R,
 	max(s.SI_WSE_median) SI_WSE_median, 
 	s.HI, 
 	avg(s.std) std, avg(s.mean) mean
---into [STAGING_2].[dbo].XXX_ITG_Sell_IN_Periods
-from  [STAGING_2].[dbo].XXX_ITG_Sell_IN t	      
+--into [STAGING_2].[dbo].XXX_P_ITG_Sell_IN_Periods
+from  [STAGING_2].[dbo].XXX_P_ITG_Sell_IN t	      
 left join ITG_Sell_IN_std  s
 	   on --n.CAL_DATE is not null and 
 		  t.[CUSTOMER_ID]=s.[CUSTOMER_ID] and  
@@ -221,7 +188,7 @@ select  t.r R,
 	t.Midcategory,
 	ceiling( t.SI_MRKT_ajust) SI_MRKT_ajust,
 	ceiling( t.SI_ITG_WSE) SI_ITG_WSE
-into [STAGING_2].[dbo].XXX_ITG_Sell_IN_Periods
+into [STAGING_2].[dbo].XXX_P_ITG_Sell_IN_Periods
 from ITG_Sell_IN_top t
 	join	ITG_Sell_IN_top p 
 	on   t.[CUSTOMER_ID] = p.[CUSTOMER_ID] and
